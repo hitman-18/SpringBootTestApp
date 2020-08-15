a5eff3eec8ce10dc9b1d15fef29d055ecd586f64
Sumit <Sumit@DESKTOP-N5D2FIA>
2020-08-15 18:51:33 +0530

    WH-9887 fourth commit

--- src/main/java/in/nareshit/raghu/runner/MessageRunner.java
+++ src/main/java/in/nareshit/raghu/runner/MessageRunner.java
@@ -1,9 +1,14 @@
 package in.nareshit.raghu.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 
 public class MessageRunner implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(MessageRunner.class);
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
