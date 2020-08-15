526782d8ef40c41b2481c52b9208fc819519b318
Sumit <Sumit@DESKTOP-N5D2FIA>
2020-08-15 18:33:29 +0530

    988 - second commit

--- src/main/java/in/nareshit/raghu/runner/MessageRunner.java
+++ src/main/java/in/nareshit/raghu/runner/MessageRunner.java
@@ -7,7 +7,7 @@ public class MessageRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello");
 	}
 
 }
