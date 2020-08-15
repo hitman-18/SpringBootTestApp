0b2874b91788d03875dc5721ff29c3008fc7503d
Sumit <Sumit@DESKTOP-N5D2FIA>
2020-08-15 14:51:02 +0530

    WH - 8520 First time file update

---
+++ src/main/java/in/nareshit/raghu/runner/MessageRunner.java
@@ -0,0 +1,13 @@
+package in.nareshit.raghu.runner;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
